52e54896df66cd8cdad73bb17a6d5f89cd7de366
ACAPP-dev <acapp909@gmail.com>
2020-03-17 15:55:56 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories, reject_if: proc {|attributes| attributes[:name].empty?}
+  #accepts_nested_attributes_for :categories, reject_if: proc {|attributes| attributes[:name].empty?}
 
+  def categories_attributes=(category_hash)
+    if !category_hash["0"][:name].empty?
+      category = Category.find_or_create_by(category_hash["0"])
+      self.post_categories.build(category: category)
+    end
+  end
 end
